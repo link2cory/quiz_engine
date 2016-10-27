267085c1b6577bdfe1ba59cbd15eb1c179403f07
Cory Perkins <Link2cory@gmail.com>
2016-10-27 16:15:32 -0700

    implemented more of the functional test spec (modified the spec slightly to accommodate a small change in the desired behavior)

--- functional_tests.py
+++ functional_tests.py
@@ -12,26 +12,48 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_quiz_and_retrieve_it_later(self):
-        # John had heard of a cool new online quiz app.  He goes to check out its
-        # homepage
+        # John had heard of a cool new online quiz app.  He goes to check out
+        # its homepage
         self.browser.get('http://localhost:8000')
 
         # He notices the page title and header mention quizes
-        self.assertIn('Quiz', self.browser.title)
-        self.fail('Finish the test!')
+        self.assertIn('Quiz Time!', self.browser.title)
 
-        # He is invited to answer a multiple-choice question right away
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Quiz Time!', header_text)
 
-        # He clicks one of the four answers, then submits his answer by clicking a
-        # submit button underneath
+        # He is invited to answer a multiple-choice question with 4 possible
+        # answers right away
+        # the question contains question text, 4 possible answers, and a submit
+        # button
+        question = self.browser.find_element_by_id('id_question')
+        question_text = question.find_element_by_class_name('question_text')
+        question_answers = question.find_elements_by_class_name('answer')
+        submit_button = question.find_element_by_class_name('submit')
 
-        # The page updates, and the original question and answers are replaced by a
-        # second question.  Above these, there is text informing John of the
-        # correctness of his answer
+        self.assertEqual(len(question_answers), 4)
+        # He clicks one of the four answers, it is now the selected answer
+        question_answers[0].click()
 
-        # John wonders whether the site will remember his progress if he leaves and
-        # returns to the site.  Then he sees that the site has generated a unique URL
-        # for him -- there is some explanatory text to that effect.
+        # John then submits his answer by clicking the submit button
+        submit_button.click()
+
+        # The page updates, and the original question and answers are replaced
+        # by a second question.  Above these, there is text informing John of
+        # the correctness of his answer
+        result = self.browser.find_element_by_id('id_result')
+        result_text = result.find_element_by_class_name('result_text')
+        question = self.browser.find_element_by_id('id_question')
+        question_text = question.find_element_by_class_name('question_text')
+        question_answers = question.find_elements_by_class_name('answer')
+        submit_button = question.find_element_by_class_name('submit')
+
+        self.assertEqual(len(question_answers), 4)
+        self.fail("finish the test!")
+        # John wonders whether the site will remember his progress if he
+        # leaves and returns to the site.  Then he sees that the site has
+        # generated a unique URL for him -- there is some explanatory text to
+        # that effect.
 
         # He visits that URL - the second quiz question is still there.
 
