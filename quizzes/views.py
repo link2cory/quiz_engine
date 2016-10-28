168f3f4355421c2788fa668a03a9d12186b1fd8e
Cory Perkins <Link2cory@gmail.com>
2016-10-28 16:55:38 -0700

    modified the basic quiz view and its associated template to display a result based on the previous question

--- quizzes/views.py
+++ quizzes/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 
 
 def home_page(request):
+    result = ''
     if request.method == 'POST':
-        return HttpResponse('Correct!')
-    return render(request, 'home.html')
+        answer = request.POST.get('answer', False)
+        if (answer == "John"):
+            result = 'Correct!'
+        else:
+            result = 'Wrong!'
+    return render(request, 'home.html', {'result': result})
