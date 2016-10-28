d8d3aec0cc47f897e61ef9d25eb23fd8c9340bd2
Cory Perkins <Link2cory@gmail.com>
2016-10-28 13:30:15 -0700

    added csrf token to the home template.  Had to add a special extension of TestCase to handle this

--- quizzes/tests.py
+++ quizzes/tests.py
@@ -5,15 +5,31 @@ from django.http import HttpRequest
 
 from quizzes.views import home_page
 
+import re
 
-class HomePageTest(TestCase):
 
-    def test_root_url_resolbes_to_home_page_view(self):
+class MyTestCase(TestCase):
+
+    @staticmethod
+    def remove_csrf(html_code):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', html_code)
+
+    def assertEqualExceptCSRF(self, html_code1, html_code2):
+        return self.assertEqual(
+            self.remove_csrf(html_code1),
+            self.remove_csrf(html_code2)
+        )
+
+
+class HomePageTest(MyTestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(expected_html, response.content.decode())
+        expected_html = render_to_string('home.html', request=request)
+        self.assertEqualExceptCSRF(expected_html, response.content.decode())
